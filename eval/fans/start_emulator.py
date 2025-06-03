6ea285d4cd70c2a8c8c17d16a204c6bfcae0a5e4
philipp <philipp.mao@epfl.ch>
2025-06-03 18:19:56 +0200

    adsfasf

--- eval/fans/start_emulator.py
+++ eval/fans/start_emulator.py
@@ -4,6 +4,6 @@ import os
 BASE_PATH = os.path.dirname(__file__)
 
 sys.path.append(os.path.join(BASE_PATH, "..", ".."))
-import emluator.emulator as emulator
+import emulator.emulator as emulator
 
-emulator.start_emulator(sys.argv[1])
+emulator.full_reset(sys.argv[1])
--- eval/fans/test_fuzz.sh
+++ eval/fans/test_fuzz.sh
@@ -4,6 +4,7 @@ export META_TARGET=aarch64emu28
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+python3 "$SCRIPT_DIR/start_emulator.py" emulator-5554
 
 python3 "$SCRIPT_DIR/../../instrument/interface.py" -d emulator-5554 -s wificond 
 python3 "$SCRIPT_DIR/../../fuzz/orchestrate.py" -d emulator-5554 -s wificond -t 120
