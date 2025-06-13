d7e6a634d99fc67dcd413a25026be6429e6759fa
philipp <philipp.mao@epfl.ch>
2025-06-13 16:45:03 +0200

    more docker

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 if [ "$(dpkg --print-architecture)" = "arm64" ]; then
 	cd fans && tar xvf _frida.abi3.so.tar.gz && cd ..
-	echo "COPY fans/_frida.abi3.so /usr/local/lib/python3.12/dist-packages/frida/_frida.abi3.so" >> Dockerfile
+	cp Dockerfile Dockerfile_fans
+	echo "COPY fans/_frida.abi3.so /usr/local/lib/python3.12/dist-packages/frida/_frida.abi3.so" >> Dockerfile_fans
+	docker build . -t nass --file Dockerfile_fans
+	exit 0
 fi
 
 docker build . -t nass
